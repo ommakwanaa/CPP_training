463c48d26debc00df40680dbb54a8f134504761d
ommakwanaa <om.makwana@einfochips.com>
2022-02-23 00:07:05 +0530

    deploying

--- 22_02_2022/operator_overloading_in_class.cpp
+++ 22_02_2022/operator_overloading_in_class.cpp
@@ -1,68 +1,55 @@
 #include<iostream>
+
 using namespace std;
-class complex{
-    int real, img;
+class class2;
+class class1{
+    int a, b;
     public:
-        complex(int a=0, int b=0){
-            real = a;
-            img = b;
+        class1(int x=0, int y=0){
+            a = x;
+            b = y;
+        }
+        
+        void getData(){
+            cout<<"a is: "<<a<<" and b is: "<<b<<endl;
         }
 
-        // method to cout the object
-        void display(){
-            cout<< "real = "<<real<<endl<<"imaginary = "<<img<<endl<<endl;
-        }
-
-        // operator overloading using friend friend function
-        friend complex operator+(complex a, complex b){
-            complex temp;
-            temp.real = a.real + b.real;
-            temp.img = a.img + b.img;
-            return temp;
-        }
-
-        friend complex operator-(complex a, complex b){
-            complex temp;
-            temp.real = a.real - b.real;
-            temp.img = a.img - b.img;
-            return temp;
-        }
-
-        friend complex operator*(complex a, complex b){
-            complex temp;
-            temp.real = a.real * b.real;
-            temp.img = a.img * b.img;
-            return temp;
-        }
-
-        friend complex operator/(complex a, complex b){
-            complex temp;
-            temp.real = a.real / b.real;
-            temp.img = a.img / b.img;
-            return temp;
-        }
+        friend class1 operator+(class1 c1, class2 c2);
 };
 
+class class2{
+    int a, b;
+    public:
+        class2(int x=0, int y=0){
+            a = x;
+            b = y;
+        }
+        void sum(){
+            cout<< "addition of a and b is " << a + b << endl;
+        }
+        void getData(){
+            cout<<"a is: "<<a<<" and b is: "<<b<<endl;
+        }
+        friend class1 operator+(class1 c1, class2 c2);
+};
+
+class1 operator+(class1 c1, class2 c2){
+    class1 temp;
+    temp.a = c1.a + c2.a;
+    temp.b = c1.b + c2.b;
+    return temp;
+}
+
 int main(){
-    complex c1(10,40), c2(10,10), c3, c4;
-    c1.display();
-    c2.display();
+    class1 c1(10,20);
+    c1.getData();
 
-    // method-2 to invoke the friend function
-    cout<<"after addition operator overloading"<<endl;
-    c4 = operator+(c1, c2);
-    c4.display();
+    class2 c2(30,40);
+    c2.getData();
 
-    cout<<"after subtraction operator overloading"<<endl;
-    c4 = operator-(c1, c2);
-    c4.display();
-
-    cout<<"after multiplication operator overloading"<<endl;
-    c4 = operator*(c1, c2);
-    c4.display();
-
-    cout<<"after division operator overloading"<<endl;
-    c4 = operator/(c1, c2);
-    c4.display();
+    class1 c3 = c1 + c2; //first method to friend function invoke operator overloading
+    // class1 c3 = operator+(c1, c2); //second method to friend function invoke operator overloading
+    c3.getData();
+    
     return 0;
 }
